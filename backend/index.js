bf6c162fd0a5e917f088fe4ebf50aea4eb48d4df
natali.dahary <natali.dahary@s.afeka.ac.il>
2025-07-16 12:36:40 +0300

    sync package-lock.json with package.json

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5002;
+
+app.use(cors({ origin: '*' }));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const NOTES_FILE = path.join(__dirname, 'notes.json');
+
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(NOTES_FILE, 'utf-8'));
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(NOTES_FILE, 'utf-8'));
+    const newNote = { id: Date.now(), text: req.body.text };
+    notes.push(newNote);
+    fs.writeFileSync(NOTES_FILE, JSON.stringify(notes, null, 2));
+    res.status(201).json(newNote);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
